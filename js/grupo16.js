461e564a61a47e6e1ba11375f8d734a4edb66a9c
palomariera <54947291+palomariera@users.noreply.github.com>
2021-06-30 23:49:43 -0300

    animacion monito integrada
    integre la animacion del monito. Tambien esta flipeada pero desde otro png. El cazador nose porq desaparece, supongo q tiene q ver con algo de la actualizacion del monito pero la verdad no estoy segura.

--- js/grupo16.js
+++ js/grupo16.js
@@ -1,6 +1,7 @@
 //variables globales
 var canvas, ctx;
-var puntos=0, vidas=3;
+var puntos = 0,
+    vidas = 3;
 var intervalo;
 
 //Variables físicas
@@ -9,7 +10,11 @@ var gravedad = 0.5;
 var velocidadCazador = 1;
 
 //Posiciones fondos
-var posA=0, posB=0, posC=0, posD=0, posE=0; 
+var posA = 0,
+    posB = 0,
+    posC = 0,
+    posD = 0,
+    posE = 0;
 
 var posPlataformaUno;
 var posPlataformaDos;
@@ -25,7 +30,7 @@ var imgBananaOro = new Image();
 var imgBananaPodrida = new Image();
 
 //SRC Imagenes
-var srcImgMono = 'img/mono_sprite.png';
+var srcImgMono = 'img/monoSpritemono.png';
 var srcImgCazador = 'img/cazador_sprite.png';
 var srcImgPlataforma = 'img/plataforma.png';
 var srcImgPlataformaDos = 'img/plataforma_2.png';
@@ -37,61 +42,63 @@ var posXCazador = 25;
 var posYCazador = 280;
 
 //Creacion Instancias
-var mono = new Personaje(posXMono, posYMono, 99, 116);
+var mono = new Personaje(posXMono, posYMono, 465, 116, 4, 2);
 var cazador = new Personaje(posXCazador, posYCazador, 67, 137);
 
 
 
 //canvas
-function dibujar(){
-	document.getElementById('canvas').style.backgroundImage = "url(img/fondo_00.png), url(img/fondo_01.png), url(img/fondo_02.png), url(img/fondo_03.png), url(img/fondo_04.png)"
+function dibujar() {
+    document.getElementById('canvas').style.backgroundImage = "url(img/fondo_00.png), url(img/fondo_01.png), url(img/fondo_02.png), url(img/fondo_03.png), url(img/fondo_04.png)"
     document.getElementById('canvas').style.backgroundPosition = "0px 0px, 0px 0px, 0px 0px, 0px 0px, 0px 0px"
-	canvas = document.getElementById('canvas');
-	ctx = canvas.getContext('2d');
-		
-	imgPersonajeMono.src = srcImgMono;
-	imgPersonajeMono.onload = function(){
-		ctx.drawImage(imgPersonajeMono, posXMono, posYMono);
-	}
-    imgPersonajeMonoIzq.src = srcImgMono;
-    imgPersonajeMonoIzq.style.transform = "scaleX(-1)";
-    
-	imgPersonajeCazador.src = srcImgCazador;
-	imgPersonajeCazador.onload = function(){
-		ctx.drawImage(imgPersonajeCazador, posXCazador, posYCazador)
-	}
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
-    intervalo = setInterval(function(){
+    imgPersonajeMono.src = srcImgMono;
+    imgPersonajeMono.onload = function() {
+        ctx.drawImage(imgPersonajeMono, posXMono, posYMono);
+    }
+    imgPersonajeMonoIzq.src = "img/monoSpritemono_flipped.png"; //por el momento flipee la imagen desde archivo, despues vemos si lo podemos hacer desde aca
+    imgPersonajeMonoIzq.style.transform = "scaleX(-1)";
+
+    imgPersonajeCazador.src = srcImgCazador;
+    imgPersonajeCazador.onload = function() {
+        ctx.drawImage(imgPersonajeCazador, posXCazador, posYCazador)
+    }
+
+    intervalo = setInterval(function() {
         borrar();
-        if(vidas==0){
-			ctx.font="100px impact";
-			ctx.fillStyle="#000000";
-			ctx.fillText('FIN', 350,300);
-        }else{
+
+        if (vidas == 0) {
+            ctx.font = "100px impact";
+            ctx.fillStyle = "#000000";
+            ctx.fillText('FIN', 350, 300);
+        } else {
             mono.velocidad += gravedad;
             mono.y += mono.velocidad;
-            if (mono.y > 410 - mono.alto){
+            if (mono.y > 410 - mono.alto) {
                 frenarSalto(410)
             }
-            if (mono.orientacion == "der"){
+            if (mono.orientacion == "der") {
                 mono.dibujar(imgPersonajeMono);
-            }else{
+            } else {
                 mono.dibujar(imgPersonajeMonoIzq);
+                console.log("voy izq");
             }
-            
+            mono.actualizar();
             cazador.dibujar(imgPersonajeCazador);
         }
-    }, 1000/25);
+    }, 1000 / 25);
 }
 
-function frenarSalto(ubicacionY){
+function frenarSalto(ubicacionY) {
     mono.velocidad = 0;
     mono.y = ubicacionY - mono.alto;
     mono.saltando = false;
 }
 
 //Creacion Objeto Personaje
-function Personaje(x, y, ancho, alto){
+function Personaje(x, y, ancho, alto, fotogramasTotales, tiempoPorFotograma) { // x,y, ancho, alto, fotogramas totales, framerate
     this.x = x;
     this.y = y;
     this.alto = alto;
@@ -99,34 +106,72 @@ function Personaje(x, y, ancho, alto){
     this.orientacion = "der";
     this.velocidad = 0;
     this.saltando = false;
+
+    //atributos animacion personaje
+    this.fotogramasTotales = fotogramasTotales;
+    this.tiempoPorFotograma = tiempoPorFotograma;
+    this.contador = 0;
+    this.fotogramaActual = 0;
+
     //metodos
-    this.dibujar = function(img){
-        ctx.drawImage(img, this.x, this.y, this.ancho, this.alto)
-    }
-    this.derecha = function(){
-        if (this.x == 350) {
-            posA -= 7;
-            posB = posA*0.5;
-            posC = posB*0.5;
-            posD = posC*0.5;
-            posE = posD*0.5;
-            canvas.style.backgroundPosition = posA+"px 0px, "+posB+"px 0px, "+posC+"px 0px, "+posD+"px 0px, "+posE+"px 0px";
-        }else{
-        this.x += 7;
-        this.orientacion = "der"
+    //metodo actualizar los fotogramas de animacion
+    this.actualizar = function() {
+
+        this.contador += 1;
+
+        if (this.contador > tiempoPorFotograma) {
+
+            this.contador = 0;
+
+            // va pasando de fotogramas
+            if (this.fotogramaActual < fotogramasTotales - 1) {
+                //ir al fotograma siguiente
+                this.fotogramaActual += 1;
+            } else {
+                //sino el siguiente es el primer fotograma
+                this.fotogramaActual = 0;
+            }
         }
     }
-    this.izquierda = function(){
-        if (this.x >= 30){
+
+
+    this.dibujar = function(img) {
+        ctx.drawImage(
+            img,
+            this.fotogramaActual * this.ancho / this.fotogramasTotales,
+            0,
+            this.ancho / this.fotogramasTotales,
+            this.alto,
+            this.x,
+            this.y,
+            this.ancho / this.fotogramasTotales,
+            this.alto);
+    }
+    this.derecha = function() {
+        if (this.x == 350) {
+            // movimiento del fondo
+            posA -= 7;
+            posB = posA * 0.5;
+            posC = posB * 0.5;
+            posD = posC * 0.5;
+            posE = posD * 0.5;
+            canvas.style.backgroundPosition = posA + "px 0px, " + posB + "px 0px, " + posC + "px 0px, " + posD + "px 0px, " + posE + "px 0px";
+        } else {
+            this.x += 7;
+            this.orientacion = "der"
+        }
+    }
+    this.izquierda = function() {
+        if (this.x >= 30) {
             this.x -= 7;
             this.orientacion = "izq"
         }
     }
-    this.saltar = function(){
+    this.saltar = function() {
         if (this.saltando == false) {
             this.saltando = true;
             this.velocidad -= velocidad * 4.5;
-        }else{
+        } else {
             if (this.y == 300) {
                 this.saltando = false;
                 this.velocidad = 0;
@@ -137,44 +182,44 @@ function Personaje(x, y, ancho, alto){
 }
 
 //Objeto Plataformas
-function Plataforma (x, y, ancho, alto){
+function Plataforma(x, y, ancho, alto) {
     this.x = x;
     this.y = y;
     this.alto = alto;
     this.ancho = ancho;
-    this.dibujar = function(img){
+    this.dibujar = function(img) {
         ctx.drawImage(img, this.x, this.y, this.ancho, this.alto)
     }
 }
 //Objeto Bananas
-function Banana (x, y, tipo, ancho, alto){
+function Banana(x, y, tipo, ancho, alto) {
     this.x = x;
     this.y = y;
     this.tipo = tipo;
     this.alto = alto;
     this.ancho = ancho;
-    this.dibujar = function(img){
+    this.dibujar = function(img) {
         ctx.drawImage(img, this.x, this.y, this.ancho, this.alto)
     }
 }
 
-function borrar(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+function borrar() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
 
 //Esc a menu
-document.addEventListener('keyup', function(e){
-if(e.keyCode==27){
-console.log("Salir a menu")
-}
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+        console.log("Salir a menu")
+    }
 });
 
 //Inputs Movimiento personaje
-document.addEventListener('keydown', function(e){
-//    console.log(e)
-    switch(e.keyCode){
+document.addEventListener('keydown', function(e) {
+    //    console.log(e)
+    switch (e.keyCode) {
         case 38:
             mono.saltar();
             break;
@@ -197,10 +242,10 @@ document.addEventListener('keydown', function(e){
             mono.izquierda();
             break;
     }
-    });
+});
 
-    //Mouse Menu [cambiar document por el id del boton que pulse]
-document.addEventListener('click', function(o){
-//    console.log(o)
+//Mouse Menu [cambiar document por el id del boton que pulse]
+document.addEventListener('click', function(o) {
+    //    console.log(o)
     console.log("Click")
-    });
+});
